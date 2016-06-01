8d3713ab67ddd720aec72c18b9dc8ea3642cc98f
Ignasi Oliver <ignasioliverf@gmail.com>
2016-06-01 23:54:30 +0200

    Create second.cc

---
+++ 2016-UPF-Contest/Draft/second.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N, i, nsubjects, subjectsiterator, subject;
+    cin >> N;
+    for ( i = 0; i < N; i++ ){
+        cin >> name;
+        cin >> nsubjects;
+        int subjectsiterator;
+        for ( subjectsiterator = 0; subjectsiterator < nsubjects; subjectsiterator++ ){
+            cin >> subject;
+        }
+    }
+    int N2, i2;
+    cin >> N2;
+    for ( i2 = 0; i2 < N2; i2++ ){
+        cin >> interorunion;
+    
+    
+}
+    
+//LETS DO THIS with two matrixes
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    int MATRIX[]
+
